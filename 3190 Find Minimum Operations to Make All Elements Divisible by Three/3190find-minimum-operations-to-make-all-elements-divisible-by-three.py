480a37febb2bc74474d24dc8b70f24b202568bf0
Linda Ng <lindang603@gmail.com>
2024-06-22 14:33:43 +0000

    Code challenge submission for question: 3190 Find Minimum Operations to Make All Elements Divisible by Three

---
+++ 3190 Find Minimum Operations to Make All Elements Divisible by Three/3190find-minimum-operations-to-make-all-elements-divisible-by-three.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        res = 0
+        
+        for num in nums:
+            if num % 3 != 0:
+                res += 1
+        
+        return res
+
+#Time: O(n)
+#Space: O(1)
