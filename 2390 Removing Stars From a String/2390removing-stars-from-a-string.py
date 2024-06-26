6fc58159a9f1f3ae8efb98ad30669264cea31874
Linda Ng <lindang603@gmail.com>
2024-06-26 19:12:06 +0000

    Code challenge submission for question: 2390 Removing Stars From a String

---
+++ 2390 Removing Stars From a String/2390removing-stars-from-a-string.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        res = []
+
+        for char in s:
+            if char == "*":
+                res.pop()
+            else:
+                res.append(char)
+        
+        return "".join(res)
+
+#Time: O(n)
+#Space: O(n)
+
