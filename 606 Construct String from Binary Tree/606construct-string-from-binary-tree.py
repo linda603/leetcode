64245745fab3dc75149a86136a8e73b638398e86
Linda Ng <lindang603@gmail.com>
2024-08-12 19:54:42 +0000

    Code challenge submission for question: 606 Construct String from Binary Tree

---
+++ 606 Construct String from Binary Tree/606construct-string-from-binary-tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        res = []
+
+        def dfs(node):
+            if not node:
+                return
+            res.append("(")
+            res.append(str(node.val))
+            if not node.left and node.right:
+                res.append("()")
+            dfs(node.left)
+            dfs(node.right)
+            res.append(")")
+        
+        dfs(root)
+        return "".join(res[1:-1])
+
+#Time: O(n)
+#Space: O(h)
