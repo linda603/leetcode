2a410ff587d37959c879a89f62e7f6407fbb47fa
Linda Ng <lindang603@gmail.com>
2024-10-10 18:04:59 +0000

    Code challenge submission for question: 201 Bitwise AND of Numbers Range

---
+++ 201 Bitwise AND of Numbers Range/201bitwise-and-of-numbers-range.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        cnt = 0
+
+        while left != right:
+            left >>= 1
+            right >>= 1
+            cnt += 1
+        left = left << cnt
+        return left
+
+#Time: O(log10(n))
+#Space: O(1)
