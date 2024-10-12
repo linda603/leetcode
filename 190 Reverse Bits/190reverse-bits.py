48d79b2febe682751ecaa2de7519fc912f98427e
Linda Ng <lindang603@gmail.com>
2024-10-12 19:14:23 +0000

    Code challenge submission for question: 190 Reverse Bits

---
+++ 190 Reverse Bits/190reverse-bits.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+
+        for i in range(32):
+            res <<= 1
+            res += n % 2
+            n >>= 1
+        return res
+
+# Time: O(32)
+# Space: O(1)
