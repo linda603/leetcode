324f70cc2717576df432bb424fab61499427235b
Linda Ng <lindang603@gmail.com>
2024-12-02 12:14:11 +0000

    Code challenge submission for question: 1455 Check If a Word Occurs As a Prefix of Any Word in a Sentence

---
+++ 1455 Check If a Word Occurs As a Prefix of Any Word in a Sentence/1455check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
+        words = sentence.split(" ")
+
+        for i, word in enumerate(words):
+            if word.startswith(searchWord):
+                return i + 1
+        return -1
+
+# Time: O(n)
+# Space: O(n)
