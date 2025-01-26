dab1a29a261da179c2ce110f689149446bb3eff0
Linda Ng <lindang603@gmail.com>
2025-01-26 15:44:35 +0000

    Code challenge submission for question: 209 Minimum Size Subarray Sum

---
+++ 209 Minimum Size Subarray Sum/209minimum-size-subarray-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        res = float("inf")
+        total = 0
+
+        l = 0
+        for r in range(len(nums)):
+            total += nums[r]
+            while total >= target:
+                res = min(res, r - l + 1)
+                total -= nums[l]
+                l += 1
+        return res if res != float("inf") else 0
+
+# Time: O(n)
+# Space: O(1)
