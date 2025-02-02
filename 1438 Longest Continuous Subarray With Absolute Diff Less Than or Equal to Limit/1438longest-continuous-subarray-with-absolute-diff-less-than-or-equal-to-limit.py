0016032a6d45c2707c853ad18f8ad83290c5dc63
Linda Ng <lindang603@gmail.com>
2025-02-02 22:54:08 +0000

    Code challenge submission for question: 1438 Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit

---
+++ 1438 Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit/1438longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -0,0 +1,26 @@
+class Solution:
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        max_queue = collections.deque() # monotonic decreasing
+        min_queue = collections.deque() # monotonic increasing
+        res = 0
+
+        l = 0
+        for r in range(len(nums)):
+            while max_queue and nums[max_queue[-1]] < nums[r]:
+                max_queue.pop()
+            while min_queue and nums[min_queue[-1]] > nums[r]:
+                min_queue.pop()
+            max_queue.append(r)
+            min_queue.append(r)
+
+            while l < r and nums[max_queue[0]] - nums[min_queue[0]] > limit:
+                l += 1
+                if max_queue[0] < l:
+                    max_queue.popleft()
+                if min_queue[0] < l:
+                    min_queue.popleft()
+            res = max(res, r - l + 1)
+        return res
+
+# Time: O(n)
+# Space: O(2n)
