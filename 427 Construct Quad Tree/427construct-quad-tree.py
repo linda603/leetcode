fb332ff3e7fc8c37599daab7ca6d25250ad0d4d8
Linda Ng <lindang603@gmail.com>
2025-02-12 16:36:16 +0000

    Code challenge submission for question: 427 Construct Quad Tree

--- 427 Construct Quad Tree/427construct-quad-tree.py
+++ 427 Construct Quad Tree/427construct-quad-tree.py
@@ -12,27 +12,27 @@ class Node:
 
 class Solution:
     def construct(self, grid: List[List[int]]) -> 'Node':
+
+        def same_val(r, c, n):
+            val = grid[r][c]
+
+            for i in range(r, r + n):
+                for j in range(c, c + n):
+                    if grid[i][j] != val:
+                        return False
+            return True
         
         def dfs(r, c, n):
-            all_match = self.check_all_match(grid, r, c, n)
-            if all_match:
-                return Node(grid[r][c], True, None, None, None, None)
+            if same_val(r, c, n):
+                return Node(grid[r][c], 1, None, None, None, None)
             n = n // 2
             top_left = dfs(r, c, n)
             top_right = dfs(r, c + n, n)
             bottom_left = dfs(r + n, c, n)
             bottom_right = dfs(r + n, c + n, n)
-            return Node(grid[r][c], False, top_left, top_right, bottom_left, bottom_right)
-        
-        return dfs(0, 0, len(grid))
-    
-    def check_all_match(self, grid, r, c, n):
-        for i in range(n):
-            for j in range(n):
-                if grid[r][c] != grid[r + i][c + j]:
-                    return False
-        return True
+            return Node(grid[r][c], 0, top_left, top_right, bottom_left, bottom_right)
 
-# Time: O(logn*4*n^2) = O(nlogn). Length of every dfs reduces to half. 
-#       Every recursion, need to check all values in the grid if they have the same values.
-# Space: O(logn). Depth of dfs()
+        return dfs(0, 0, len(grid))
+
+# Time: O(n^2logn)
+# Space: O(logn)
