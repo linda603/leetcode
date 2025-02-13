29792665568f3d38815570c7072fbb5481829bb4
Linda Ng <lindang603@gmail.com>
2025-02-13 11:29:39 +0000

    Code challenge submission for question: 62 Unique Paths

--- 62 Unique Paths/62unique-paths.py
+++ 62 Unique Paths/62unique-paths.py
@@ -3,9 +3,9 @@ class Solution:
         cache = {}
 
         def dfs(r, c):
-            if r < 0 or r == m or c < 0 or c == n:
+            if r == m or c == n:
                 return 0
-            if r == m - 1 and c == n - 1:
+            if r == m - 1 or c == n - 1:
                 return 1
             if (r, c) in cache:
                 return cache[(r, c)]
@@ -13,5 +13,5 @@ class Solution:
             return cache[(r, c)]
         return dfs(0, 0)
 
-# Time: O(2^(m + n)) -> O(mn)
+# Time: O(2^mn) -> O(mn)
 # Space: O(mn)
