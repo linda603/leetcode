294e8bdd6e4ee0023bf18f3f26a33c36700be6b4
Linda Ng <lindang603@gmail.com>
2025-02-16 17:12:36 +0000

    Code challenge submission for question: 337 House Robber III

--- 337 House Robber III/337house-robber-iii.py
+++ 337 House Robber III/337house-robber-iii.py
@@ -6,18 +6,17 @@
 #         self.right = right
 class Solution:
     def rob(self, root: Optional[TreeNode]) -> int:
-        
-        def dfs(node):
-            if not node:
-                return [0, 0] # [max_with_node, max_without_node]
-            left = dfs(node.left)
-            right = dfs(node.right)
-            max_with_node = node.val + left[1] + right[1]
-            max_without_node = max(left[0], left[1]) + max(right[0], right[1])
-            return [max_with_node, max_without_node]
-        
-        res = dfs(root)
+        res = self.dfs(root)
         return max(res[0], res[1])
+    
+    def dfs(self, node):
+        if not node:
+            return [0, 0]
+        left = self.dfs(node.left)
+        right = self.dfs(node.right)
+        max_with_node = node.val + left[1] + right[1]
+        max_without_node = max(left[0], left[1]) + max(right[0], right[1])
+        return [max_with_node, max_without_node]
 
 # Time: O(n)
 # Space: O(h)
