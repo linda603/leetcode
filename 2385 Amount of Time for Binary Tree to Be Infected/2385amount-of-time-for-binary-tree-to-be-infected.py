92069628fc0ae48c34c0f8ed8b3bc801afa9cf26
Linda Ng <lindang603@gmail.com>
2025-02-16 19:55:52 +0000

    Code challenge submission for question: 2385 Amount of Time for Binary Tree to Be Infected

--- 2385 Amount of Time for Binary Tree to Be Infected/2385amount-of-time-for-binary-tree-to-be-infected.py
+++ 2385 Amount of Time for Binary Tree to Be Infected/2385amount-of-time-for-binary-tree-to-be-infected.py
@@ -5,42 +5,30 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def __init__(self):
+        self.time = 0
+
     def amountOfTime(self, root: Optional[TreeNode], start: int) -> int:
+        self.dfs(root, start)
+        return self.time
         
-        parent_map, start_node = self.get_parents(root, start)
+    def dfs(self, node, start):
+        if not node:
+            return [False, 0] # [found start node, depth from this node]
+        left = self.dfs(node.left, start)
+        right = self.dfs(node.right, start)
 
-        # bfs
-        time = 0
-        queue = deque([start_node])
-        visited = set()
+        if node.val == start:
+            self.time = max(self.time, max(left[1], right[1]))
+            return [True, 1]
+        if left[0]:
+            self.time = max(self.time, left[1] + right[1])
+            return [True, 1 + left[1]]
+        elif right[0]:
+            self.time = max(self.time, left[1] + right[1])
+            return [True, 1 + right[1]]
+        else:
+            return [False, 1 + max(left[1], right[1])] # [False, max_depth from this node to leaf node from left child + right child]
 
-        while queue:
-            size = len(queue)
-            for i in range(size):
-                node = queue.popleft()
-                visited.add(node)
-                if node in parent_map and parent_map[node] not in visited:
-                    queue.append(parent_map[node])
-                if node.left and node.left not in visited:
-                    queue.append(node.left)
-                if node.right and node.right not in visited:
-                    queue.append(node.right)
-            time += 1
-        return time - 1
-    
-    def get_parents(self, root, start):
-        parent_map = {}
-        queue = deque([root])
-
-        while queue:
-            node = queue.popleft()
-            if node.val == start:
-                start_node = node
-            if node.left:
-                parent_map[node.left] = node
-                queue.append(node.left)
-            if node.right:
-                parent_map[node.right] = node
-                queue.append(node.right)
-        return (parent_map, start_node)
-            
+# Time: O(n)
+# Space: O(1)
