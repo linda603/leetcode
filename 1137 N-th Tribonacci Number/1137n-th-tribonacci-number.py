9208ff56b65e1d3908c0424da0d17949c87dfea1
Linda Ng <lindang603@gmail.com>
2025-02-16 16:45:54 +0000

    Code challenge submission for question: 1137 N-th Tribonacci Number

--- 1137 N-th Tribonacci Number/1137n-th-tribonacci-number.py
+++ 1137 N-th Tribonacci Number/1137n-th-tribonacci-number.py
@@ -1,14 +1,15 @@
 class Solution:
+    def __init__(self):
+        self.cache = {0: 0, 1: 1, 2: 1}
+
     def tribonacci(self, n: int) -> int:
-        cache = {0: 0, 1: 1, 2: 1}
+        return self.dfs(n)
+    
+    def dfs(self, i):
+        if i in self.cache:
+            return self.cache[i]
+        self.cache[i] = self.dfs(i - 3) + self.dfs(i - 2) + self.dfs(i - 1)
+        return self.cache[i]
 
-        def dfs(i):
-            if i in cache:
-                return cache[i]
-            res = dfs(i - 3) + dfs(i - 2) + dfs(i - 1)
-            cache[i] = res
-            return res
-        return dfs(n)
-
-#Time: O(n) dfs(i) is called once
-#Space: O(n). Hash map cache contain n + 1 elements
+# Time: O(n)
+# Space: O(n)
