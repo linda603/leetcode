561f9a2e2dd5ca90795cb4c93d9977b01b65dfec
Linda Ng <lindang603@gmail.com>
2025-02-16 16:26:43 +0000

    Code challenge submission for question: 91 Decode Ways

--- 91 Decode Ways/91decode-ways.py
+++ 91 Decode Ways/91decode-ways.py
@@ -1,19 +1,21 @@
 class Solution:
-    def numDecodings(self, s: str) -> int:
-        cache = {}
-        
-        def dfs(i):
-            if i >= len(s):
-                return 1
-            if i in cache:
-                return cache[i]
-            if s[i] == "0":
-                return 0
-            cache[i] = dfs(i + 1)
-            if (i + 1 < len(s)) and (s[i] == "1" or (s[i] == "2" and s[i + 1] in "0123456")):
-                cache[i] += dfs(i + 2)
-            return cache[i]
-        return dfs(0)
+    def __init__(self):
+        self.cache = {}
 
-# Time: O(n)
+    def numDecodings(self, s: str) -> int:
+        return self.dfs(s, 0)
+    
+    def dfs(self, s, i):
+        if i >= len(s):
+            return 1
+        if s[i] == "0":
+            return 0
+        if i in self.cache:
+            return self.cache[i]
+        self.cache[i] = self.dfs(s, i + 1)
+        if i + 1 < len(s) and (s[i] == "1" or (s[i] == "2" and s[i + 1] in "0123456")):
+            self.cache[i] += self.dfs(s, i + 2)
+        return self.cache[i]
+        
+# Time: O(2^n) -> O(n)
 # Space: O(n)
