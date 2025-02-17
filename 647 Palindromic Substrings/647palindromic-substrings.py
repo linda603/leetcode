b4b6f8cdda9dedc211e53da16d9f8a9477a1a43b
Linda Ng <lindang603@gmail.com>
2025-02-17 10:56:26 +0000

    Code challenge submission for question: 647 Palindromic Substrings

--- 647 Palindromic Substrings/647palindromic-substrings.py
+++ 647 Palindromic Substrings/647palindromic-substrings.py
@@ -1,21 +1,24 @@
 class Solution:
+
     def countSubstrings(self, s: str) -> int:
         res = 0
 
         for i in range(len(s)):
-            res += self.find_pal(s, i, i) # odd pals
-            res += self.find_pal(s, i, i + 1) # even pals
+            odd_count = self.get_pal(s, i, i)
+            even_count = self.get_pal(s, i, i + 1) if i + 1 < len(s) else 0
+            res += odd_count + even_count
         return res
     
-    def find_pal(self, nums, l, r):
-        if r >= len(nums): return 0
-        res = 0
+    def get_pal(self, s, l, r):
+        count = 0
 
-        while l >= 0 and r < len(nums) and nums[l] == nums[r]:
-            res += 1
+        while l >= 0 and r < len(s):
+            if s[l] != s[r]:
+                break
             l -= 1
             r += 1
-        return res
+            count += 1
+        return count
 
 # Time: O(n^2)
 # Space: O(1)
