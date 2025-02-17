281bab9c6c7ca32dacabdb7765e3d72858547bda
Linda Ng <lindang603@gmail.com>
2025-02-17 12:47:24 +0000

    Code challenge submission for question: 212 Word Search II

--- 212 Word Search II/212word-search-ii.py
+++ 212 Word Search II/212word-search-ii.py
@@ -1,51 +1,57 @@
-class trie_node:
+class TrieNode:
     def __init__(self):
         self.children = {}
         self.end_of_word = ""
 
-class trie:
+class Trie:
     def __init__(self):
-        self.root = trie_node()
+        self.root = TrieNode()
     
     def add(self, word):
         curr = self.root
 
         for c in word:
             if c not in curr.children:
-                curr.children[c] = trie_node()
+                curr.children[c] = TrieNode()
             curr = curr.children[c]
         curr.end_of_word = word
 
 class Solution:
+    def __init__(self):
+        self.res = []
+
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
         m, n = len(board), len(board[0])
-        curr_trie = trie()
+        trie = Trie()
         for word in words:
-            curr_trie.add(word)
-
-        visited = set()
-        res = []
-        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-        def dfs(r, c, node):
-            nonlocal res, visited
-            if r < 0 or r == m or c < 0 or c == n or (r, c) in visited or board[r][c] not in node.children:
-                return
-            node = node.children[board[r][c]]
-            if node.end_of_word:
-                res.append(node.end_of_word)
-                node.end_of_word = "" # to prevent adding this word repeatedly
-            visited.add((r, c))
-            for dr, dc in directions:
-                nei_r = r + dr
-                nei_c = c + dc
-                dfs(nei_r, nei_c, node)
-            visited.remove((r, c))
-            return
-
+            trie.add(word)
+        
         for r in range(m):
             for c in range(n):
-                dfs(r, c, curr_trie.root)
-        return res
+                if board[r][c] in trie.root.children:
+                    self.dfs(board, trie, r, c, trie.root, set())
+        return self.res
+    
+    def dfs(self, board, trie, r, c, node, visited):
+        m, n = len(board), len(board[0])
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
-# Time: O(kl + mn*4^l). O(kl) to build trie. O(mn4^l) dfs() of the word
-# Space: O(kl + l) = O(kl)
+        if (r < 0 or r == m or c < 0 or c == n or board[r][c] not in node.children or
+            (r, c) in visited):
+            return
+        visited.add((r, c))
+        node = node.children[board[r][c]]
+        #print(r, c, board[r][c])
+        if node.end_of_word:
+            self.res.append(node.end_of_word)
+            node.end_of_word = ""
+        #print(self.res)
+        for dr, dc in directions:
+            nei_r = r + dr
+            nei_c = c + dc
+            self.dfs(board, trie, nei_r, nei_c, node, visited)
+        visited.remove((r, c))
+        return
+
+# Time: O(kw + mn*4^w)
+# Space: O(kw + w)
