da9569ab851be238d76f106e8dafd0ed608297fb
Linda Ng <lindang603@gmail.com>
2025-02-18 22:30:42 +0000

    Code challenge submission for question: 135 Candy

--- 135 Candy/135candy.py
+++ 135 Candy/135candy.py
@@ -1,16 +1,15 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        rewards = [1] * len(ratings)
+        candies = [1] * len(ratings)
 
         for i in range(1, len(ratings)):
             if ratings[i] > ratings[i - 1]:
-                rewards[i] = rewards[i - 1] + 1
-        
+                candies[i] = candies[i - 1] + 1
+
         for i in range(len(ratings) - 2, -1, -1):
-            if ratings[i] > ratings[i + 1] and rewards[i] <= rewards[i + 1]:
-                rewards[i] = rewards[i + 1] + 1
+            if ratings[i] > ratings[i + 1] and candies[i] <= candies[i + 1]:
+                candies[i] = candies[i + 1] + 1
+        return sum(candies)
 
-        return sum(rewards)
-
-# Time: O(3n)
+# Time: O(2n)
 # Space: O(n)
