5bdec0b8d5d90fae39ed191b937c8c0a66006a99
Linda Ng <lindang603@gmail.com>
2025-02-22 22:30:30 +0000

    Code challenge submission for question: 269 Alien Dictionary

--- 269 Alien Dictionary/269alien-dictionary.py
+++ 269 Alien Dictionary/269alien-dictionary.py
@@ -1,47 +1,52 @@
 class Solution:
+    def __init__(self):
+        self.res = []
+        self.visited = set()
+        self.cycle = set()
+        self.adj = {}
+
     def alienOrder(self, words: List[str]) -> str:
-        adj = {c: [] for word in words for c in word}
-        
+        if not self.get_adj(words):
+            return ""
+
+        for c in self.adj.keys():
+            if not self.dfs(c):
+                return ""
+        return "".join(self.res[::-1])
+    
+    def get_adj(self, words):
+        self.adj = {c: [] for word in words for c in word}
+
         for i in range(len(words) - 1):
             word1 = words[i]
             word2 = words[i + 1]
             min_len = min(len(word1), len(word2))
-            # invalid case "wrtui" before "wrt"
-            if word1[:min_len] == word2[:min_len] and len(word1) > len(word2):
-                return ""
+            # invalid case
+            if len(word1) > len(word2) and word1[:min_len] == word2:
+                return False
             for j in range(min_len):
                 if word1[j] != word2[j]:
-                    adj[word1[j]].append(word2[j])
+                    self.adj[word1[j]].append(word2[j])
                     break
-        
-        visited = set()
-        cycle = set()
-        res = []
+        return True
 
-        def dfs(c):
-            if adj[c] == []:
-                if c not in visited:
-                    res.append(c)
-                    visited.add(c)
-                return True
-            if c in cycle:
-                return False
-            cycle.add(c)
-            for nei in adj[c]:
-                if not dfs(nei):
-                    return False
-            cycle.remove(c)
-            adj[c] = []
-            res.append(c)
-            visited.add(c)
+    def dfs(self, char):
+        if self.adj[char] == []:
+            if char not in self.visited:
+                self.res.append(char)
+                self.visited.add(char)
             return True
+        if char in self.cycle:
+            return False
+        self.cycle.add(char)
+        for nei in self.adj[char]:
+            if not self.dfs(nei):
+                return False
+        self.cycle.remove(char)
+        self.res.append(char)
+        self.visited.add(char)
+        self.adj[char] = []
+        return True
 
-        for c in adj.keys():
-            if not dfs(c):
-                return ""
-        
-        return "".join(res[::-1])
-
-# Time: O(n + V + E). n: len(words) to build adj list {}. dfs() takes O(E + V)
-# Space: O(26*26 + 26 + 26) = O(1)
-        
+# Time: O(n + E + V). n: total length of words. O(n) to build adj list
+# Space: O(E + V + V + V)
