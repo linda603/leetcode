c06c1e59361561cf70fd03104461de8c0ebddaa0
Linda Ng <lindang603@gmail.com>
2025-02-24 21:53:32 +0000

    Code challenge submission for question: 621 Task Scheduler

--- 621 Task Scheduler/621task-scheduler.py
+++ 621 Task Scheduler/621task-scheduler.py
@@ -1,20 +1,25 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        time = 0
-        count = Counter(tasks)
-        heap = [-cnt for cnt in count.values()]
+        freq = {}
+
+        for task in tasks:
+            if task not in freq:
+                freq[task] = 0
+            freq[task] += 1
+        heap = [-count for count in freq.values()]
         heapq.heapify(heap)
-        queue = collections.deque()
+        queue = deque()
 
+        time = 0
         while heap or queue:
-            time += 1
             if heap:
-                cnt = heapq.heappop(heap)
-                if cnt + 1 != 0:
-                    queue.append((time + n, cnt + 1))
-            while queue and queue[0][0] == time:
-                heapq.heappush(heap, queue.popleft()[1])
-        return time 
+                count = heapq.heappop(heap)
+                if count + 1 != 0: # still need to process this task later after time + n
+                    queue.append((count + 1, time + n))
+            if queue and queue[0][1] == time:
+                heapq.heappush(heap, queue.popleft()[0])
+            time += 1
+        return time
 
-# Time: O(n + 26 + 26log26 + 26log26) = O(n)
-# Space: O(26 + 26)
+# Time: O(n + 26 + 26log26) = O(n)
+# Space: O(26 + 26) = O(1)
