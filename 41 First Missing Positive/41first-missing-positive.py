741bebcae626367838f3f4d825c9f018a861d600
Linda Ng <lindang603@gmail.com>
2025-02-25 22:21:51 +0000

    Code challenge submission for question: 41 First Missing Positive

--- 41 First Missing Positive/41first-missing-positive.py
+++ 41 First Missing Positive/41first-missing-positive.py
@@ -1,24 +1,11 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        for i, num in enumerate(nums):
-            if num < 0:
-                nums[i] = 0
-        
-        for num in nums:
-            i = abs(num) - 1
-            if i >= 0 and i < len(nums):
-                if nums[i] > 0:
-                    nums[i] *= -1
-                elif nums[i] == 0:
-                    nums[i] = -1 * (len(nums) + 1) # change to negative that does not affect to the solution
-                else: # nums[i] < 0 -> i + 1 already exist
-                    continue
-    
-        for num in range(1, len(nums) + 1):
-            i = num - 1
-            if nums[i] >= 0:
-                return num
-        return len(nums) + 1
+        # all intergers we need to check are from range [1, len(nums) + 1]
+        nums_set = set(nums)
 
-#Time: O(3n)
-#Space: O(1)
+        for val in range(1, len(nums) + 2):
+            if val not in nums_set:
+                return val
+
+# Time: O(n)
+# Space: O(n)
