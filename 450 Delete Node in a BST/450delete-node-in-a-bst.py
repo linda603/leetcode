8e04e52dda6f46894f6ac2c2923293a70269bc80
Linda Ng <lindang603@gmail.com>
2025-02-25 21:21:31 +0000

    Code challenge submission for question: 450 Delete Node in a BST

--- 450 Delete Node in a BST/450delete-node-in-a-bst.py
+++ 450 Delete Node in a BST/450delete-node-in-a-bst.py
@@ -7,10 +7,10 @@
 class Solution:
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
         if not root:
-            return root
+            return None
         if root.val == key:
             return self.remove_node(root)
-
+    
         curr = root
         while curr:
             # if the node is found, correct the pointers
@@ -27,27 +27,23 @@ class Solution:
                 else:
                     curr = curr.right
         return root
-    
-    def find_most_left(self, node):
-        curr = node
-
-        while curr.left:
-            curr = curr.left
-        return curr
 
     def remove_node(self, node):
         if not node.left:
             return node.right
         if not node.right:
             return node.left
-  
-        # Find the most left (smallest element in the right sub tree)
-        most_left = self.find_most_left(node.right)
-        # every node in the left subtree is smaller than the right subtree
-        most_left.left = node.left
-        return node.right
+        # find the most right of left child tree
+        most_right = self.find_most_right(node.left)
+        # every node in the right child is greater than left child
+        most_right.right = node.right
+        return node.left
     
+    def find_most_right(self, node):
+        curr = node
+        while curr.right:
+            curr = curr.right
+        return curr
 
-# Time: O(h) = O(logn) to find node has key val + find most left node
+# Time: O(h) = O(logn)
 # Space: O(1)
-                
