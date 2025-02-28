2e9cc5fcc914dc93b9d089d93d854304928e83af
Linda Ng <lindang603@gmail.com>
2025-02-28 09:45:40 +0000

    Code challenge submission for question: 684 Redundant Connection

--- 684 Redundant Connection/684redundant-connection.py
+++ 684 Redundant Connection/684redundant-connection.py
@@ -1,36 +1,36 @@
+class UnionFind:
+    def __init__(self, n):
+        self.par = [i for i in range(n + 1)]
+        self.rank = [1] * (n + 1)
+    
+    def find(self, x):
+        p = self.par[x]
+
+        while p != self.par[p]:
+            p = self.par[self.par[p]]
+            p = self.par[p]
+        return p
+    
+    def union(self, a, b):
+        par_a = self.find(a)
+        par_b = self.find(b)
+
+        if par_a == par_b:
+            return False
+        if self.rank[par_a] >= self.rank[par_b]:
+            self.rank[par_a] += self.rank[par_b]
+            self.par[par_b] = par_a
+        else:
+            self.rank[par_b] += self.rank[par_a]
+            self.par[par_a] = par_b
+        return True
+
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        n = len(edges)
-        par = [i for i in range(n + 1)]
-        rank = [1] * (n + 1)
-
-        def find(x):
-            p = par[x]
-
-            while p != par[p]:
-                p = par[par[p]]
-                p = par[p]
-            return p
-        
-        def union(a, b):
-            pa = find(a)
-            pb = find(b)
-
-            if pa == pb:
-                return False
-            if rank[pa] >= rank[pb]:
-                rank[pa] += rank[pb]
-                par[pb] = pa
-            else:
-                rank[pb] += rank[pa]
-                par[pa] = pb
-            return True
-        
+        union_find = UnionFind(len(edges))
         for a, b in edges:
-            if not union(a, b):
+            if not union_find.union(a, b):
                 return [a, b]
 
-# Time: O(E*V). E = V in this problem. Normally E = V - 1.
-#.      Traverse all edges to union O(E)
-#       Union takes O(V)
-# Space: O(V)
+# Time: O(E*V). O(E) to union all edges. O(V) to find parent of the node
+# Space: O(V + V)
