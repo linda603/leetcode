e03aba5963ae1a5d3bfcb39e693bd768c7920e0b
Linda Ng <lindang603@gmail.com>
2025-02-28 10:24:32 +0000

    Code challenge submission for question: 778 Swim in Rising Water

--- 778 Swim in Rising Water/778swim-in-rising-water.py
+++ 778 Swim in Rising Water/778swim-in-rising-water.py
@@ -1,24 +1,20 @@
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        heap = [[grid[0][0], 0, 0]] # time, r, c
-        visited = set()
+        heap = [(grid[0][0], 0, 0)]
+        visited = set([(0, 0)])
         directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
 
         while heap:
-            time, r, c = heapq.heappop(heap)
+            depth, r, c = heapq.heappop(heap)
             if r == n - 1 and c == n - 1:
-                return time
-            if (r, c) in visited:
-                continue
-            visited.add((r, c))
+                return depth
             for dr, dc in directions:
                 nei_r = r + dr
                 nei_c = c + dc
-                if nei_r in range(n) and nei_c in range(n) and (nei_r, nei_c) not in visited:
-                    new_time = max(time, grid[nei_r][nei_c])
-                    heapq.heappush(heap, [new_time, nei_r, nei_c])
-        return -1
+                if 0 <= nei_r < n and 0 <= nei_c < n and (nei_r, nei_c) not in visited:
+                    heapq.heappush(heap, (max(grid[nei_r][nei_c], depth), nei_r, nei_c))
+                    visited.add((nei_r, nei_c))
 
-# Time: O(n^2logn^2) = O(n^2logn)
+# Time: O(n^2logn^2)
 # Space: O(n^2)
