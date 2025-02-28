e3ef158b9b78a9b24974b899c93dff0413137833
Linda Ng <lindang603@gmail.com>
2025-02-28 09:34:21 +0000

    Code challenge submission for question: 1584 Min Cost to Connect All Points

--- 1584 Min Cost to Connect All Points/1584min-cost-to-connect-all-points.py
+++ 1584 Min Cost to Connect All Points/1584min-cost-to-connect-all-points.py
@@ -1,32 +1,31 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        if len(points) == 1:
-            return 0
         adj = {i: [] for i in range(len(points))}
 
         for i in range(len(points)):
             x1, y1 = points[i]
             for j in range(i + 1, len(points)):
                 x2, y2 = points[j]
-                adj[i].append((abs(x1 - x2) + abs(y1 - y2), j))
-                adj[j].append((abs(x1 - x2) + abs(y1 - y2), i))
+                dist = abs(x1 - x2) + abs(y1 - y2)
+                adj[i].append((dist, j))
+                adj[j].append((dist, i))
         
-        heap = [(0, 0)] # cost, point 0
-        total = 0
+        heap = [(0, 0)] # (dist, node)
         visited = set()
 
-        while len(visited) < len(points):
-            cost, point = heapq.heappop(heap)
-            if point in visited:
+        total = 0
+        while heap:
+            dist, node = heapq.heappop(heap)
+            if node in visited:
                 continue
-            visited.add(point)
-            total += cost
+            visited.add(node)
+            total += dist
             if len(visited) == len(points):
                 return total
-            for nei_cost, nei in adj[point]:
-                if nei not in visited:
-                    heapq.heappush(heap, (nei_cost, nei))
-            
-# Time: O(V^2 + ElogE) = O(V^2 + ElogV^2) = O(V^2 + ElogV)
-# Space: O(E). O(E) due to adj() + O(E) due to heap
+            for nei_dist, nei in adj[node]:
+                if nei in visited:
+                    continue
+                heapq.heappush(heap, (nei_dist, nei))
 
+# Time: O(n^2 + n^2logn^2). n: len(points). n^2 = E
+# Space: O(n^2)
