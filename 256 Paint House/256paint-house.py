136f07327a3296e6ec2ce963249c28a939cea50b
Linda Ng <lindang603@gmail.com>
2025-03-02 19:31:30 +0000

    Code challenge submission for question: 256 Paint House

--- 256 Paint House/256paint-house.py
+++ 256 Paint House/256paint-house.py
@@ -1,18 +1,14 @@
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
-        res = 0
-        cache = {}
+        prev_cost = costs[len(costs) - 1]
 
-        def dfs(i, prev):
-            if i >= len(costs):
-                return 0
-            if (i, prev) in cache:
-                return cache[(i, prev)]
-            cache[(i, prev)] = float("inf")
-            for color in range(3):
-                if color == prev:
-                    continue
-                cache[(i, prev)] = min(cache[(i, prev)], costs[i][color] + dfs(i + 1, color))
-            return cache[(i, prev)]
-        
-        return dfs(0, -1)
+        for i in range(len(costs) - 2, -1, -1): # i is the house      
+            cost_house_0 = costs[i][0] + min(prev_cost[1], prev_cost[2])
+            cost_house_1 = costs[i][1] + min(prev_cost[0], prev_cost[2])
+            cost_house_2 = costs[i][2] + min(prev_cost[0], prev_cost[1])
+
+            prev_cost = [cost_house_0, cost_house_1, cost_house_2]
+        return min(prev_cost)
+
+# Time: O(n)
+# Space: O(1)
