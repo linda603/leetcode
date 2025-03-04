ec062a5bffcbab3f338411a1836af46053b0bd10
Linda Ng <lindang603@gmail.com>
2025-03-04 10:19:34 +0000

    Code challenge submission for question: 207 Course Schedule

--- 207 Course Schedule/207course-schedule.py
+++ 207 Course Schedule/207course-schedule.py
@@ -4,25 +4,24 @@ class Solution:
 
         for course, preq in prerequisites:
             adj[course].append(preq)
-
-        visited = set()
-        def dfs(node):
-            if adj[node] == []:
-                return True
-            if node in visited:
-                return False
-            visited.add(node)
-            for nei in adj[node]:
-                if not dfs(nei):
-                    return False
-            visited.remove(node)
-            adj[node] = []
-            return True
         
-        for node in range(numCourses):
-            if not dfs(node):
+        for i in range(numCourses):
+            if not self.dfs(adj, i, set()):
                 return False
         return True
+    
+    def dfs(self, adj, course, visited):
+        if adj[course] == []:
+            return True
+        if course in visited:
+            return False
+        visited.add(course)
+        for nei in adj[course]:
+            if not self.dfs(adj, nei, visited):
+                return False
+        adj[course] = []
+        visited.remove(course)
+        return True
 
-# Time: O(V + E + V + E) = O(V + E)
-# Space: O(V + E + V)
+# Time: O(E + V + E + V)
+# Space: O(E + V)
