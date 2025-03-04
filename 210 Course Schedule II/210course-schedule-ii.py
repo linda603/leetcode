0be73862f65ad5820cb9e2e3b0cfc07d4abb3e5a
Linda Ng <lindang603@gmail.com>
2025-03-04 12:20:11 +0000

    Code challenge submission for question: 210 Course Schedule II

--- 210 Course Schedule II/210course-schedule-ii.py
+++ 210 Course Schedule II/210course-schedule-ii.py
@@ -1,38 +1,28 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        adj = {i: [] for i in range(numCourses)}
-        res = []
+        adj = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
 
         for course, preq in prerequisites:
             adj[course].append(preq)
+            indegree[preq] += 1
         
-        visited = set()
-        cycle = set()
-
-        def dfs(course):
-            if adj[course] == []:
-                if course not in visited:
-                    res.append(course)
-                    visited.add(course)
-                return True
-            if course in cycle:
-                return False
-            cycle.add(course)
-            for nei in adj[course]:
-                if not dfs(nei):
-                    return False
-            cycle.remove(course)
-            res.append(course)
-            visited.add(course)
-            adj[course] = []
-            return True
-
+        queue = deque()
         for course in range(numCourses):
-            # detect a cycle, cannot finish the course
-            if not dfs(course):
-                return []
-        return res
-
-# Time: O(E + V + E + V)
-# Space: O(E + V + V + V). adj map: O(E + V), visited set O(n)
+            if indegree[course] == 0:
+                queue.append(course)
         
+        res = []
+
+        while queue:
+            course = queue.popleft()
+            res.append(course)
+            for nei in adj[course]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    queue.append(nei)
+
+        return res[::-1] if len(res) == numCourses else []
+
+# Time: O(E + V + E + V + V)
+# Space: O(E + V)
