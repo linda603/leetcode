37b50533b8af17fd1053f13e1e9e0d0a31dd28a2
Linda Ng <lindang603@gmail.com>
2025-03-04 21:01:49 +0000

    Code challenge submission for question: 99 Recover Binary Search Tree

--- 99 Recover Binary Search Tree/99recover-binary-search-tree.py
+++ 99 Recover Binary Search Tree/99recover-binary-search-tree.py
@@ -15,22 +15,18 @@ class Solution:
         Do not return anything, modify root in-place instead.
         """
         self.dfs(root)
-        tmp = self.first_violation.val
-        self.first_violation.val = self.second_violation.val
-        self.second_violation.val = tmp
-    
-    # inorder traversal
+        self.first_violation.val, self.second_violation.val = self.second_violation.val, self.first_violation.val
+
     def dfs(self, node):
         if not node:
             return
-        self.dfs(node.left)
-        # processing at curr node, check is violation happens
-        if self.prev and node.val < self.prev.val:
+        left = self.dfs(node.left)
+        if self.prev and self.prev.val > node.val:
             if not self.first_violation:
                 self.first_violation = self.prev
             self.second_violation = node
         self.prev = node
-        self.dfs(node.right)
+        right = self.dfs(node.right)
 
 # Time: O(n)
 # Space: O(h)
