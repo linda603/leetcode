668fea19f86be5be769b03292050dc862e81690d
Linda Ng <lindang603@gmail.com>
2025-03-10 20:33:41 +0000

    Code challenge submission for question: 564 Find the Closest Palindrome

--- 564 Find the Closest Palindrome/564find-the-closest-palindrome.py
+++ 564 Find the Closest Palindrome/564find-the-closest-palindrome.py
@@ -1,45 +1,38 @@
 class Solution:
     def nearestPalindromic(self, n: str) -> str:
-        all_pos = []
-        
-        if len(n) % 2:
-            is_odd = True
-            mid = len(n) // 2
-        else:
-            is_odd = False
-            mid = len(n) // 2 - 1
-        half = int(n[: mid + 1])
+        string = list(n)
+        total_len = len(string)
+        is_odd = total_len % 2
+        half = total_len // 2 + 1 if is_odd else total_len // 2
+        all_pos = [0] * 5
 
-        all_pos.append(self.half_to_pal(half, is_odd))
-        all_pos.append(self.half_to_pal(half - 1, is_odd))
-        all_pos.append(self.half_to_pal(half + 1, is_odd))
-        all_pos.append(10 ** (len(n) - 1) - 1)
-        all_pos.append(10 ** len(n) + 1)
-    
-        print(all_pos)
+        all_pos[0] = self.get_pal(int("".join(string[:half])) - 1, is_odd)
+        all_pos[1] = self.get_pal(int("".join(string[:half])), is_odd)
+        all_pos[2] = self.get_pal(int("".join(string[:half])) + 1, is_odd)
+        all_pos[3] = 10 ** (total_len - 1) - 1
+        all_pos[4] = 10 ** total_len + 1
+        #print(all_pos)
+
         res = float("inf")
         min_diff = float("inf")
         for num in all_pos:
             if num == int(n):
                 continue
-            curr = abs(num - int(n))
-            if curr < min_diff:
-                min_diff = curr
+            curr_diff = abs(num - int(n))
+            if curr_diff < min_diff:
                 res = num
-            elif curr == min_diff:
-                res = min(res, num)
+                min_diff = curr_diff
+            elif curr_diff == min_diff and num < res:
+                res = num
         return str(res)
     
-    def half_to_pal(self, half, is_odd):
-        res = half
+    def get_pal(self, num, is_odd):
+        string = list(str(num))
 
         if is_odd:
-            half = half // 10
-        
-        while half > 0:
-            res = res * 10 + half % 10
-            half = half // 10
-        return res
-
-# Time: O(5*n/2) = O(n)
-# Space: O(n) due to building first half = n[: mid + 1]
+            return int("".join(string + string[:-1][::-1]))
+        else:
+            return int("".join(string + string[::-1]))
+    
+# Time: O(log10(n))
+# Space: O(log10(n)) due to build string[:half]
