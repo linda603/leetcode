628c5ff92099854edb5ba33a07acb948336a4bb4
Linda Ng <lindang603@gmail.com>
2025-03-14 22:10:20 +0000

    Code challenge submission for question: 981 Time Based Key-Value Store

--- 981 Time Based Key-Value Store/981time-based-key-value-store.py
+++ 981 Time Based Key-Value Store/981time-based-key-value-store.py
@@ -1,32 +1,32 @@
 class TimeMap:
 
     def __init__(self):
-        self.stores = {}
+        self.map = {}
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.stores:
-            self.stores[key] = []
-        self.stores[key].append([timestamp, value])
-        
-    def get(self, key: str, timestamp: int) -> str:
-        if key not in self.stores:
-            return ""
-        lists = self.stores[key]
+        if key not in self.map:
+            self.map[key] = []
+        self.map[key].append((timestamp, value))
 
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.map:
+            return ""
+        records = self.map[key]
         l = 0
-        r = len(lists) - 1
+        r = len(records) - 1
         res = ""
 
         while l <= r:
             mid = (l + r) // 2
-            if lists[mid][0] <= timestamp:
-                res = lists[mid][1]
+            if records[mid][0] <= timestamp:
+                res = records[mid][1]
                 l = mid + 1
             else:
                 r = mid - 1
         return res
 
-# Time: O(1) for set(), O(logn) for get()
+
+# Time: O(logn)
 # Space: O(n)
 
 # Your TimeMap object will be instantiated and called as such:
