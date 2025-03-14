b058d032214a555f3b34c175a5823bf6510f3ee2
Linda Ng <lindang603@gmail.com>
2025-03-14 12:59:01 +0000

    Code challenge submission for question: 1101 The Earliest Moment When Everyone Become Friends

--- 1101 The Earliest Moment When Everyone Become Friends/1101the-earliest-moment-when-everyone-become-friends.py
+++ 1101 The Earliest Moment When Everyone Become Friends/1101the-earliest-moment-when-everyone-become-friends.py
@@ -1,33 +1,46 @@
+class UnionFind:
+    def __init__(self, n):
+        self.par = [i for i in range(n)]
+        self.rank = [1] * n
+        self.edges = 0
+    
+    def find(self, i):
+        p = self.par[i]
+
+        while p != self.par[p]:
+            p = self.par[self.par[p]]
+            p = self.par[p]
+        return p
+
+    def union(self, a, b):
+        par_a = self.find(a)
+        par_b = self.find(b)
+
+        if par_a == par_b:
+            return False
+        if self.rank[par_a] >= self.rank[par_b]:
+            self.rank[par_a] += self.rank[par_b]
+            self.par[par_b] = par_a
+        else:
+            self.rank[par_b] += self.rank[par_a]
+            self.par[par_a] = par_b
+        self.edges += 1
+        return True
+
+    def get_edges(self):
+        return self.edges
+
+
 class Solution:
     def earliestAcq(self, logs: List[List[int]], n: int) -> int:
+        union_f = UnionFind(n)
         logs.sort(key=lambda x:x[0])
-        par = [i for i in range(n)]
-        edges = 0
-
-        def find(x):
-            p = par[x]
-
-            while p != par[p]:
-                p = par[par[p]]
-                p = par[p]
-            return p
-
-        def union(a, b):
-            nonlocal edges
-            pa = find(a)
-            pb = find(b)
-
-            if pa == pb:
-                return
-            par[pb] = pa
-            edges += 1
-            return
 
         for timestamp, a, b in logs:
-            union(a, b)
-            if edges == n - 1:
+            union_f.union(a, b)
+            if union_f.get_edges() == n - 1:
                 return timestamp
         return -1
 
-# Time: O(mlogm + n + mn). m: len(logs)
-# Space: O(m + n)
+# Time: O(nlogn)
+# Space: O(n)
