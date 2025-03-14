04e7004581dd33490621d6ffb34225bdc4666793
Linda Ng <lindang603@gmail.com>
2025-03-14 10:10:40 +0000

    Code challenge submission for question: 3355 Zero Array Transformation I

--- 3355 Zero Array Transformation I/3355zero-array-transformation-i.py
+++ 3355 Zero Array Transformation I/3355zero-array-transformation-i.py
@@ -1,18 +1,17 @@
 class Solution:
     def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
-        events = [0] * (len(nums) + 1)
-        
+        line_sweep = [0] * (len(nums) + 1)
+
         for start, end in queries:
-            events[start] += 1
-            events[end + 1] -= 1
+            line_sweep[start] += 1
+            line_sweep[end + 1] -= 1
         
         count = 0
         for i, num in enumerate(nums):
-            count += events[i]
+            count += line_sweep[i]
             if count < num:
-                print("i, num, count:", i, num, count)
                 return False
         return True
-
+    
 # Time: O(q + n)
 # Space: O(n)
