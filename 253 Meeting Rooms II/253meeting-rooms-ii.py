21f57a25f3403f3163cc986e0f241d35ba87ef42
Linda Ng <lindang603@gmail.com>
2025-03-15 14:46:46 +0000

    Code challenge submission for question: 253 Meeting Rooms II

--- 253 Meeting Rooms II/253meeting-rooms-ii.py
+++ 253 Meeting Rooms II/253meeting-rooms-ii.py
@@ -1,7 +1,7 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         intervals.sort()
-        heap = [] # earliest avail time of meeting rooms
+        heap = []
 
         for start, end in intervals:
             if heap and heap[0] <= start:
@@ -9,6 +9,6 @@ class Solution:
             heapq.heappush(heap, end)
         return len(heap)
 
-# Time: O(nlogn + n) = O(nlogn)
-# space: O(n + n) = O(n)
+# Time: O(nlogn)
+# Space: O(n)
 
