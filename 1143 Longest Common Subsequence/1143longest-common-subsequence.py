cfaf69be361399de0fd68067e8fb633d0d2e4f71
Linda Ng <lindang603@gmail.com>
2025-03-15 19:38:18 +0000

    Code challenge submission for question: 1143 Longest Common Subsequence

--- 1143 Longest Common Subsequence/1143longest-common-subsequence.py
+++ 1143 Longest Common Subsequence/1143longest-common-subsequence.py
@@ -1,19 +1,21 @@
 class Solution:
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        res = []
-        cache = {}
+    def __init__(self):
+        self.cache = {}
 
-        def dfs(i, j):
-            if i >= len(text1) or j >= len(text2):
-                return 0
-            if (i, j) in cache:
-                return cache[(i, j)]
-            if text1[i] == text2[j]:
-                cache[(i, j)] = 1 + dfs(i + 1, j + 1)
-            else:
-                cache[(i, j)] = max(dfs(i + 1, j), dfs(i, j + 1))
-            return cache[(i, j)]
-        return dfs(0, 0)
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        return self.dfs(text1, text2, 0, 0)
+    
+    def dfs(self, text1, text2, i, j):
+        if i >= len(text1) or j >= len(text2):
+            return 0
+        if (i, j) in self.cache:
+            return self.cache[(i, j)]
+        self.cache[(i, j)] = 0
+        if text1[i] == text2[j]:
+            self.cache[(i, j)] = 1 + self.dfs(text1, text2, i + 1, j + 1)
+        else:
+            self.cache[(i, j)] = max(self.dfs(text1, text2, i + 1, j), self.dfs(text1, text2, i, j + 1))
+        return self.cache[(i, j)]
 
 # Time: O(mn)
 # Space: O(mn)
