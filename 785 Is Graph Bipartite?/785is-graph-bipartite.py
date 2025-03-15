8eab5458cfeb5cb3d5df078bdefce48b5d9571df
Linda Ng <lindang603@gmail.com>
2025-03-15 14:44:34 +0000

    Code challenge submission for question: 785 Is Graph Bipartite?

--- 785 Is Graph Bipartite?/785is-graph-bipartite.py
+++ 785 Is Graph Bipartite?/785is-graph-bipartite.py
@@ -1,28 +1,26 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        n = len(graph)
-        colors = [-1] * n # 0: even, 1: odd
+        colors = [-1] * len(graph) # 0: even, 1: odd
 
-        def bfs(node):
-            if colors[node] != -1:
-                return True
-            queue = deque([node])
-            while queue:
-                node = queue.popleft()
-                if colors[node] == -1:
-                    colors[node] = 0
-                for nei in graph[node]:
-                    if colors[nei] != -1 and colors[nei] == colors[node]:
-                        return False
-                    elif colors[nei] == -1:
-                        queue.append(nei)
-                        colors[nei] = not colors[node]
-            return True
-        
-        for node in range(n):
-            if not bfs(node):
+        for node in range(len(graph)):
+            if not self.bfs(graph, colors, node):
                 return False
         return True
 
-# Time: O(V(V + E))
-# Space: O(V + V)
+    def bfs(self, graph, colors, node):
+        queue = deque([node])
+
+        while queue:
+            node = queue.popleft()
+            if colors[node] == -1:
+                colors[node] = 0
+            for nei in graph[node]:
+                if colors[nei] != -1 and colors[nei] == colors[node]:
+                    return False
+                elif colors[nei] == -1:
+                    colors[nei] = not colors[node]
+                    queue.append(nei)
+        return True
+
+# Time: O(V*(E + V)))
+# Space: O(V)
