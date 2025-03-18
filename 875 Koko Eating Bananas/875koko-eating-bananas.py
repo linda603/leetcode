43d081352215940fd236ecd08de87b08aae664f4
Linda Ng <lindang603@gmail.com>
2025-03-18 20:14:00 +0000

    Code challenge submission for question: 875 Koko Eating Bananas

--- 875 Koko Eating Bananas/875koko-eating-bananas.py
+++ 875 Koko Eating Bananas/875koko-eating-bananas.py
@@ -1,24 +1,24 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
         l = 1
-        r = sum(piles)
-        res = -1
+        r = max(piles)
+        res = r
 
         while l <= r:
             mid = (l + r) // 2
-            if self.is_valid(piles, h, mid):
+            if self.can_finish(piles, h, mid):
                 res = mid
                 r = mid - 1
             else:
                 l = mid + 1
         return res
+    
+    def can_finish(self, piles, h, speed):
+        hours = 0
 
-    def is_valid(self, piles, h, speed):
-        cnt = 0
+        for pile in piles:
+            hours += math.ceil(pile / speed)
+        return hours <= h
 
-        for p in piles:
-            cnt += math.ceil(p / speed)
-        return cnt <= h
-
-# Time: O(log(sum(piles)))
+# Time: O(log(max(piles)))
 # Space: O(1)
