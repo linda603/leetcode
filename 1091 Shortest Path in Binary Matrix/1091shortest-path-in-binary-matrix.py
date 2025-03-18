b1a89b6e236aa638a0788bdf96a8c053b900680f
Linda Ng <lindang603@gmail.com>
2025-03-18 21:57:30 +0000

    Code challenge submission for question: 1091 Shortest Path in Binary Matrix

--- 1091 Shortest Path in Binary Matrix/1091shortest-path-in-binary-matrix.py
+++ 1091 Shortest Path in Binary Matrix/1091shortest-path-in-binary-matrix.py
@@ -3,26 +3,25 @@ class Solution:
         n = len(grid)
         if grid[0][0] or grid[n - 1][n - 1]:
             return -1
-        queue = collections.deque([(0, 0)])
+
+        queue = deque([(0, 0)])
+        visited = set([(0, 0)])
+
         level = 1
         directions = [[0, -1], [-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1]]
-
-        visited = set([(0, 0)])
         while queue:
             for i in range(len(queue)):
                 r, c = queue.popleft()
                 if r == n - 1 and c == n - 1:
                     return level
                 for dr, dc in directions:
-                    nei_r = r + dr
-                    nei_c = c + dc
-                    if nei_r in range(n) and (nei_c) in range(n) and grid[nei_r][nei_c] == 0 and (nei_r, nei_c) not in visited:
-                        queue.append((nei_r, nei_c))
+                    nei_r = dr + r
+                    nei_c = dc + c
+                    if 0 <= nei_r < n and 0 <= nei_c < n and grid[nei_r][nei_c] == 0 and (nei_r, nei_c) not in visited:
                         visited.add((nei_r, nei_c))
+                        queue.append((nei_r, nei_c))
             level += 1
         return -1
 
-# Time: O(mn)
-# Space: O(mn)
-
-            
+# Time: O(n^2)
+# Space: O(n^2)
