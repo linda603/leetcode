c8089c0aab3ff412a05ff15b7dfaf87881c23220
Linda Ng <lindang603@gmail.com>
2025-03-18 20:18:36 +0000

    Code challenge submission for question: 1277 Count Square Submatrices with All Ones

--- 1277 Count Square Submatrices with All Ones/1277count-square-submatrices-with-all-ones.py
+++ 1277 Count Square Submatrices with All Ones/1277count-square-submatrices-with-all-ones.py
@@ -1,26 +1,16 @@
 class Solution:
     def countSquares(self, matrix: List[List[int]]) -> int:
-        cache = {} # (r, c): max_length of square
         m, n = len(matrix), len(matrix[0])
         count = 0
 
-        def dfs(r, c):
-            nonlocal count
-            if r >= m or c >= n:
-                return 0
-            if (r, c) in cache:
-                return cache[(r, c)]
-            cache[(r, c)] = 0
-            down = dfs(r + 1, c)
-            right = dfs(r, c + 1)
-            diag = dfs(r + 1, c + 1)
+        for r in range(m - 1, -1, -1):
+            for c in range(n - 1, -1, -1):
+                if matrix[r][c] == 1:
 
-            if matrix[r][c] == 1:
-                cache[(r, c)] = 1 + min(down, right, diag)
-                count += cache[(r, c)]
-            return cache[(r, c)]
-        dfs(0, 0)
+                    if r != m - 1 and c != n - 1:
+                        matrix[r][c] += min(matrix[r + 1][c], matrix[r][c + 1], matrix[r + 1][c + 1])
+                    count += matrix[r][c]
         return count
 
 # Time: O(mn)
-# Space: O(mn)
+# Space: O(1)
