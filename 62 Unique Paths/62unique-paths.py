2c28eaeb200f4649483a2ceb36fd18685011db8d
Linda Ng <lindang603@gmail.com>
2025-03-19 19:47:52 +0000

    Code challenge submission for question: 62 Unique Paths

--- 62 Unique Paths/62unique-paths.py
+++ 62 Unique Paths/62unique-paths.py
@@ -1,17 +1,12 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        cache = {}
+        dp = [0] * (n + 1)
+        dp[n - 1] = 1
 
-        def dfs(r, c):
-            if r == m or c == n:
-                return 0
-            if r == m - 1 or c == n - 1:
-                return 1
-            if (r, c) in cache:
-                return cache[(r, c)]
-            cache[(r, c)] = dfs(r + 1, c) + dfs(r, c + 1)
-            return cache[(r, c)]
-        return dfs(0, 0)
+        for r in range(m - 1, -1, -1):
+            for c in range(n - 1, -1, -1):
+                dp[c] += dp[c + 1]
+        return dp[0]
 
-# Time: O(2^mn) -> O(mn)
-# Space: O(mn)
+# Time: O(mn)
+# Space: O(n)
